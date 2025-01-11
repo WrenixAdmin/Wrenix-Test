a22fe994529b7b14879613a645692eeb8b1abf6c
Tharindu Dasantha <t.dasanthaedirisinghe@gmail.com>
2025-01-11 22:00:16 +0530

    Added the analitics

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+// Adding the analitics
+import { Analytics } from "@vercel/analytics/react"
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -29,6 +32,9 @@ export default function RootLayout({
       >
         {children}
       </body>
+
+
+      <Analytics />
     </html>
   );
 }
