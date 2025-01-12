28d90e73a231081867fba16289c8987d4b1384dc
Tharindu Dasantha <t.dasanthaedirisinghe@gmail.com>
2025-01-12 19:10:56 +0530

    Add complete Mobile Responsiveness

--- web/src/app/coming-soon/page.tsx
+++ web/src/app/coming-soon/page.tsx
@@ -36,7 +36,7 @@ const ComingSoonPage: React.FC = () => {
                             className="text-[#484848] text-center font-ar-one-sans text-[5.5vw] font-medium leading-[3.125rem]">
                             It<span className="text-[#FFBF18]">’</span>s a New Year
                         </p>
-                        <h1 className={`text-center font-inter md:text-[29.5vw] leading-[21.875rem] -mt-[10vh] ${styles.heading}`}>
+                        <h1 className={`text-center font-inter md:text-[29.5vw] leading-[21.875rem] -mt-[8vh] ${styles.heading}`}>
                             Wrenix
                         </h1>
                     </div>
@@ -52,6 +52,35 @@ const ComingSoonPage: React.FC = () => {
                         </p>
                     </div>
                 </div>
+            </div>
+
+            {/* Mobile Display Version */}
+            <div className="lg:hidden flex flex-col h-screen p-[2rem] justify-between items-center">
+                <Image src={logo} alt={'logo'} width={200} height={150}/>
+                <div className="flex flex-col items-center gap-[5rem]">
+                    <div
+                        className="flex flex-col items-center text-center font-ar-one-sans text-[15vw] text-normal font-medium gap-[1rem]">
+                        <h1 className="text-[#FFBF18] ">New <span className="text-[#4F48EC]">Face</span></h1>
+                        <h1 className="text-[#484848] leading-10">For</h1>
+                        <h1 className="text-[#FFBF18]">New <span className="text-[#4F48EC]">Year</span></h1>
+                    </div>
+                    <div
+                        className="flex px-2.5 py-2 items-center gap-2 rounded-full bg-[#100E34] shadow-md cursor-default pointer-events-none">
+                        <div
+                            className="flex px-1 py-1 flex-col justify-center items-center gap-2.5 rounded-full bg-white">
+                            <MdEmail className="text-[#100e34] text-2xl"/>
+                        </div>
+                        <div className="flex justify-center items-center">
+                            <p className="text-white text-center font-ar-one-sans text-[1rem] font-medium leading-[0.9375rem] pointer-events-none">
+                                hello@wrenix.com
+                            </p>
+                            <MdNavigateNext className="text-white text-2xl"/>
+                        </div>
+                    </div>
+                </div>
+                <p className="text-[#2A2A2A] text-center font-inter text-[0.9rem] font-medium leading-normal">
+                    All Right Reserved by Wrenix | © Wrenix 2025
+                </p>
             </div>
         </div>
     );
